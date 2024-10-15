829d51104800025fff26146d474479117330bd17
abdullahsuleman <abdullahsuleman61@gmail.com>
2024-10-15 01:00:03 +0100

    add: staking contract

---
+++ contracts/AmboStaking.sol
@@ -0,0 +1,175 @@
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BGB##BBGP#&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&B#&#&BJ!!5B##&&@@@@&GG&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@GY?7~~~~!!~~~~777YBBBBBGGP!~7G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@GJ!~~~~~~~~~~~~~!~~!J!~~~~~~5?~~!5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B#G!!~~!77!~~~~~~~P&?~~~~~~!7?7!~77~7B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B&@@!!JG&@@@&BPY?JG&@&YJYPB&@@@&G7?J!?!Y@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@P@@@&?#@@@@@@@@@@@@@@@@@@@@@@@@@@@@B!77!!5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@5G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@GG7!77G@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@&?@@@@#@@@@@@@@&@@@@@@@@@@@@@@@&@@@@@@@Y!7!GG&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@GB@#Y?J@@@@@@@&J@@@@@@@@@@@@@&5B@@@@@@&777!5&#@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@P#P77?Y@@@@@@@@G5B&&@@@@@@&BY5&@@@@@@@&?7777GB@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@BP77?Y5G&@@@@@@@&PY55P5P5Y5P#@@@@@@@@&G5J?5YJB@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@&Y????77?#@@@@@@@@&&##GGB&&&@@@@@@@#Y777JPG#Y&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@P?????????P&@@@@@@@@@@@@@@@@@@@@BJ7??????J5P@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@Y?JJJ??JP&@@@@@@&@@@@@@@@@@@@@@&GY????JJY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5?J?YG@@@@@@@@@@&@@@@@&&@@@@@@@@@&B5JJJ5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@GY#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#JG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&B5G&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BB&@@@@@@@@@@@@@@@@@@@@@@@B5YG&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#GB&@@@@@@@@@@@@@@@@@#G5G&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@@&#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@&&#&&@@@&&@@@@&&@@@&&&&&&&@@&&&&&&&@@&&&&&&@@@&&&&&&&&@@@@&&#&@@@@@&&&&&&@@@@&&&@@@@@&&@@@@@@@@
+// @@@#J?J55JG@B7?G@@#7?5@G??J5555@#??J5555@#??JYY?JG&YYJ???YJ#@PJ?YY??Y#@&???YYJJP@&??Y@@@&J???#@@@@@@
+// @@@G??YGBB&@B??YPP5??5@G??5GGP&@B??YGGP#@#??5@&Y?7#@@#??5@@@J??&@@@5?7#&??Y@@5?7B&??Y@@&J?PG??&@@@@@
+// @@@@##BGJ??&B??YBBP??5@G??5BBB&@B??YBBB#@#??JJ??JB@@@#??5@@@Y??#@@@Y??&&???JJJYG@&??Y@@Y??5P??J&@@@@
+// @@@BJY55JJ5@B??G@@&??5@G??JYYYY@#??JYYYY&#??5@G??B@@@#??5@@@@GJ?JJ??5#@&??Y@@@@@@&??5@Y?J####Y?J&@@@
+// @@@@@&&&&@@@@&&@@@@&&@@@&@&&&&&@@&@&&&&&@@&&@@@@&&@@@@&@@@@@@@@@&&&@@@@@@@@@@@@@@@&@@@&@@@@@@@@&@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakingPlatform is ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public stakingToken;
+    IERC20 public lpToken;
+
+    struct StakingOption {
+        uint256 duration;
+        uint256 apr;
+    }
+
+    StakingOption[] public stakingOptions; //array for staking option list
+
+    struct Stake {
+        uint256 amount;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 optionIndex;
+    }
+
+    mapping(address => Stake) public stakes;
+
+    event Staked(
+        address indexed user,
+        uint256 amount,
+        uint256 duration,
+        uint256 apr
+    );
+    event Withdrawn(address indexed user, uint256 amount, uint256 reward);
+
+    constructor(address _stakingToken, address _lpToken) Ownable(msg.sender) {
+        stakingToken = IERC20(_stakingToken);
+        lpToken = IERC20(_lpToken);
+
+        // Initialize staking options
+        stakingOptions.push(StakingOption(30 seconds, 30)); // 3 months, 30% APR
+        stakingOptions.push(StakingOption(1 minutes, 50)); // 6 months, 50% APR
+        stakingOptions.push(StakingOption(5 minutes, 90)); // 12 months, 90% APR
+    }
+
+    function stake(uint256 _amount, uint256 _optionIndex)
+        external
+        nonReentrant
+    {
+        require(_amount > 0, "Cannot stake 0");
+        require(_optionIndex < stakingOptions.length, "Invalid option index");
+        require(
+            stakes[msg.sender].amount == 0,
+            "You already have an active stake"
+        );
+
+        StakingOption memory option = stakingOptions[_optionIndex];
+         // Check user's token balance
+        require(stakingToken.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
+
+        // Check allowance
+        require(stakingToken.allowance(msg.sender, address(this)) >= _amount, "Insufficient allowance");
+        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
+
+        stakes[msg.sender] = Stake({
+            amount: _amount,
+            startTime: block.timestamp,
+            endTime: block.timestamp + option.duration,
+            optionIndex: _optionIndex
+        });
+
+        require(lpToken.balanceOf(address(this)) >= _amount, "Insufficient LP tokens in contract");
+        // Mint LP tokens to the user
+        uint256 lpAmount = _amount; // 1:1 ratio for simplicity, can be adjusted
+        lpToken.safeTransfer(msg.sender, lpAmount);
+
+        emit Staked(msg.sender, _amount, option.duration, option.apr);
+    }
+
+    function withdraw() external nonReentrant {
+        Stake storage userStake = stakes[msg.sender];
+        require(userStake.amount > 0, "No active stake");
+        require(block.timestamp >= userStake.endTime, "Stake duration not met");
+
+        uint256 reward = calculateReward(msg.sender);
+        uint256 amount = userStake.amount;
+
+        delete stakes[msg.sender];
+
+        stakingToken.safeTransfer(msg.sender, amount);
+        stakingToken.safeTransfer(msg.sender, reward);
+
+        // Burn LP tokens from the user
+        lpToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        emit Withdrawn(msg.sender, amount, reward);
+    }
+
+    function calculateReward(address _user) public view returns (uint256) {
+        Stake memory userStake = stakes[_user];
+        if (userStake.amount == 0) return 0;
+
+        StakingOption memory option = stakingOptions[userStake.optionIndex];
+        uint256 stakeDuration = userStake.endTime - userStake.startTime;
+        uint256 reward = (userStake.amount * option.apr * stakeDuration) /
+            (365 days * 100);
+
+        return reward;
+    }
+
+    function getStakingOptions()
+        external
+        view
+        returns (StakingOption[] memory)
+    {
+        return stakingOptions;
+    }
+    // utility functions for ensuring the tokens management
+     function checkStakingTokenBalance(address _user) external view returns (uint256) {
+        return stakingToken.balanceOf(_user);
+    }
+
+    // Helper function to check user's allowance to this contract
+    function checkStakingTokenAllowance(address _user) external view returns (uint256) {
+        return stakingToken.allowance(_user, address(this));
+    }
+
+    // Helper function to check contract's LP token balance
+    function checkContractLPTokenBalance() external view returns (uint256) {
+        return lpToken.balanceOf(address(this));
+    }
+}
